01701a78d69d54d8b38193ffadef81d13ccf70da
nikhil patidar <patidarnikhil314@gmail.com>
2025-06-24 15:24:31 +0530

    update db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,12 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-
+console.log("DATABASE ENV CHECK:", {
+  DB: process.env.MYSQLDATABASE,
+  USER: process.env.MYSQLUSER,
+  PASS: process.env.MYSQLPASSWORD,
+  HOST: process.env.MYSQLHOST,
+  PORT: process.env.MYSQLPORT
+});
 const sequelize = new Sequelize(
   process.env.MYSQLDATABASE,     // database name
   process.env.MYSQLUSER,         // username
