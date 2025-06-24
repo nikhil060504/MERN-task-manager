afb0044177ee0951d5a3647254791f7b2a008e44
nikhil patidar <patidarnikhil314@gmail.com>
2025-06-24 14:09:58 +0530

    final .env and db update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,16 @@
-require('dotenv').config();
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.MYSQLDATABASE,     // database name
+  process.env.MYSQLUSER,         // username
+  process.env.MYSQLPASSWORD,     // password
+  {
+    host: process.env.MYSQLHOST, // host
+    port: process.env.MYSQLPORT, // port
+    dialect: 'mysql',
+    logging: false,              // optional: disable SQL query logging
+  }
+);
 
 module.exports = sequelize;
-
