22f96504bde5f8f09431059096fbd7abbe3a325c
nikhil patidar <patidarnikhil314@gmail.com>
2025-06-24 10:56:52 +0530

    correct profile controller

--- backend/controllers/profileControllers.js
+++ backend/controllers/profileControllers.js
@@ -1,43 +1,16 @@
-// const User = require("../models/User");
-
-// exports.getProfile = async (req, res) => {
-//   try {
-//     const user = await User.findById(req.user._id || req.user.id).select(
-//       "-password"
-//     );
-//     if (!user)
-//       return res.status(404).json({ status: false, msg: "User not found" });
-//     res
-//       .status(200)
-//       .json({ user, status: true, msg: "Profile found successfully.." });
-//   } catch (err) {
-//     console.error(err);
-//     return res
-//       .status(500)
-//       .json({ status: false, msg: "Internal Server Error" });
-//   }
-// };
-
-
-
-
 
 
 const User = require("../models/User");
 
 exports.getProfile = async (req, res) => {
   try {
-<<<<<<< HEAD
     const userId = req.user.id;
-
     const user = await User.findByPk(userId, {
       attributes: { exclude: ["password"] },
     });
-
     if (!user) {
       return res.status(404).json({ status: false, msg: "User not found" });
     }
-
     res.status(200).json({
       user,
       status: true,
@@ -52,21 +25,3 @@ exports.getProfile = async (req, res) => {
     });
   }
 };
-
-=======
-    const user = await User.findById(req.user._id || req.user.id).select(
-      "-password"
-    );
-    if (!user)
-      return res.status(404).json({ status: false, msg: "User not found" });
-    res
-      .status(200)
-      .json({ user, status: true, msg: "Profile found successfully.." });
-  } catch (err) {
-    console.error(err);
-    return res
-      .status(500)
-      .json({ status: false, msg: "Internal Server Error" });
-  }
-};
->>>>>>> bafbc4df1e11bab2a9e39d4807b61aaeb7b2a30d
